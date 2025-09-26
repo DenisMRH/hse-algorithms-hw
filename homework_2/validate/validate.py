e848d42891e0c60dab9b59a9f67d9f9768dbee2a
DenMac <denmac@DenMacs-MacBook-Air.local>
2025-09-26 22:05:23 +0300

    litle bit refactoring code

--- homework_2/validate/validate.py
+++ homework_2/validate/validate.py
@@ -3,26 +3,26 @@ def isability (pushed: str,poped : str) -> bool:
     pushed.reverse()
     poped = ((poped).strip()).split()
 
-    # if len(pushed) != len(poped):
-    #     return False
+    if len(pushed) != len(poped):
+        return False
 
-    temp = [None]
+    stack = [None]
+
     for i in range (0, len(poped)) :
-        if temp[len(temp)-1] != poped[i]:
-            while temp[len(temp)-1] != poped[i]  and len(pushed)>0:
-                temp.append(pushed.pop())
-                if temp[len(temp)-1] == poped[i]:
-                    print(temp[len(temp)-1])
-                    temp.pop()
+        if stack[len(stack)-1] != poped[i]:
+
+            while stack[len(stack)-1] != poped[i]  and len(pushed)>0:
+                stack.append(pushed.pop())
+
+                if stack[len(stack)-1] == poped[i]:
+                    stack.pop()
                     break
         else:
-            print(temp[len(temp)-1])
-            temp.pop()
+            stack.pop()
 
-    print(pushed,poped, temp)
-    return True if temp == [None] else  False
+    return True if stack == [None] else  False
 
-
-pushed = input("pushed: ")
-poped = input("poped: ")
-print(isability(pushed, poped))
+if __name__ == "__main__":
+    pushed = input("pushed: ")
+    poped = input("poped: ")
+    print(isability(pushed, poped))
