bc0c3d00bcebf682b60c8f3b1ba71baf93f3a9d6
DenMac <denmac@DenMacs-MacBook-Air.local>
2025-09-26 22:02:58 +0300

    first solve validate

--- homework_2/validate/validate.py
+++ homework_2/validate/validate.py
@@ -1,10 +1,28 @@
 def isability (pushed: str,poped : str) -> bool:
-    pushed = ((pushed).strip()).split()
+    pushed = (((pushed).strip()).split())
+    pushed.reverse()
     poped = ((poped).strip()).split()
-    print(pushed,poped)
 
+    # if len(pushed) != len(poped):
+    #     return False
+
+    temp = [None]
+    for i in range (0, len(poped)) :
+        if temp[len(temp)-1] != poped[i]:
+            while temp[len(temp)-1] != poped[i]  and len(pushed)>0:
+                temp.append(pushed.pop())
+                if temp[len(temp)-1] == poped[i]:
+                    print(temp[len(temp)-1])
+                    temp.pop()
+                    break
+        else:
+            print(temp[len(temp)-1])
+            temp.pop()
+
+    print(pushed,poped, temp)
+    return True if temp == [None] else  False
 
 
 pushed = input("pushed: ")
 poped = input("poped: ")
-isability(pushed, poped)
+print(isability(pushed, poped))
