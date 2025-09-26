715132992ca7500fdbd87c0f94dde363b2c89a13
DenMac <denmac@DenMacs-MacBook-Air.local>
2025-09-26 19:37:17 +0300

    init validate

---
+++ homework_2/validate/validate.py
@@ -0,0 +1,10 @@
+def isability (pushed: str,poped : str) -> bool:
+    pushed = ((pushed).strip()).split()
+    poped = ((poped).strip()).split()
+    print(pushed,poped)
+
+
+
+pushed = input("pushed: ")
+poped = input("poped: ")
+isability(pushed, poped)
